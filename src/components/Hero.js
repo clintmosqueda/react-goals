98f66e70bde7658699ff56e3322432ad8e95d004
clintmosqueda <ericmosqueda1991@gmail.com>
2020-07-18 00:50:28 +0800

    Add Slider Functionality

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,43 +6,78 @@ import Mv from '../assets/images/mv.jpg'
 
 const Hero = () => {
 
-  const [slides, setSlides] = useState();
+  const [slides, setSlides] = useState(1);
+  const [slide, setSlide] = useState(1);
 
-  console.log('number of', slides)
-
-  const bannerArticle = [
+  const bannerArticles = [
     {
       id: '1',
       image: 'image1',
-      title: 'title',
-      date: 'date'
+      title: 'サンプルテキストサンプル1',
+      date: '2019.06.19'
     },
     {
       id: '2',
       image: 'image2',
-      title: 'title2',
-      date: 'date' 
+      title: 'サンプルテキストサンプル 2',
+      date: '2019.07.19' 
     },
     {
       id: '3',
       image: 'image3',
-      title: 'title3',
-      date: 'date'
+      title: 'サンプルテキストサンプル 3',
+      date: '2019.08.19'
     }
   ]
 
   useEffect(() => {
-    setSlides(bannerArticle.length)
-  }, [bannerArticle])
+    setSlides(bannerArticles.length)
+  }, [bannerArticles.length])
 
+
+  const handlePrev = () => {
+    if(slide <= 1) {
+      setSlide(slides)
+    } else {
+      setSlide(slide - 1)
+    }
+  }
+
+  const handleNext = () => {
+    if (slide >= slides ) {
+      setSlide(1)
+    } else {
+      setSlide(slide + 1)
+    }
+  }
 
   return (
     <div className="hero">
-      <section className="hero-image" style={{backgroundImage: `url(${Mv})`}}>
-        <div className="wrapper">
-          <div className="hero-block"></div>
+      <> {bannerArticles.map((article, index) => (
+        <div className={`hero-block ${slide === index+1 ? 'is-active' : ''}`} key={index}>
+          <section className="hero-image" style={{ backgroundImage: `url(${Mv})` }}>
+            <div className="wrapper">
+              <div className="hero-content">
+                <h2 className="hero-heading">{article.title}</h2>
+                <time className="hero-time" dateTime="">{article.date}</time>
+                <time className="hero-time" dateTime="">{slide}</time>
+              </div>
+            </div>
+          </section>
         </div>
-      </section>
+      ))}
+      </>
+      <div className="hero-dots">
+        <>
+          {bannerArticles.map((dot, index) => (
+            <span className={`hero-dot ${slide === index+1 ? 'is-active' : '' }`}  key={index}></span>
+          ))}
+        </>
+      </div>
+      <div className="hero-arrows">
+        <button className="hero-arrow hero-prev" onClick={handlePrev}>&lt;</button>
+        <button className="hero-arrow hero-next" onClick={handleNext}>&gt;</button>
+      </div>
     </div>
   )
 }
